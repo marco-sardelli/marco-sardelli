98116ce357f281fabbe3c0ab9ccd021d56b80665
marco-sardelli <marco-sardelli@epitech.eu>
2021-04-16 17:44:26 +0200

    add day1 options

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+           
             url: 'https:\\www.epitech.eu',
             inventory:15,
+            selVar: 0,
             onSale: true,
-            inStock: true,
+
+            computed: {
+                image() {
+                  return this.variants[this.selVar].image
+                },
+                inStock() {
+                  return this.variants[this.selVar].quantity
+                }
+              },
+            
             details: ['50% cotton', '30% wool', '20% polyester'],
+            
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 10},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 20}
               ]
         }
     },
@@ -32,6 +43,9 @@ const app = Vue.createApp({
         changeImage(varImage){
             this.image=varImage
 
-        }
+        },
+        updateVariant(index) {
+            this.selVar = index
+          }
     }
 })
